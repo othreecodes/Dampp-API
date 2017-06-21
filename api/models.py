e77a07d947c37a8424fd33241f418b3e499a01e6
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-21 04:49:20 +0100

    Create model Profile     - Create model Story     - Add field sex to user     - Alter field full_name on user     - Alter field is_verified on user     - Alter field photo_url on user     - Add field stories to profile     - Add field user to profile

--- api/models.py
+++ api/models.py
@@ -1,10 +1,26 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from model_utils.models import TimeStampedModel
 
 
 class User(AbstractUser):
     is_verified = models.BooleanField(verbose_name='User Verified', default=False)
     photo_url = models.URLField(verbose_name='Photo Url', blank=True)
     full_name = models.CharField(max_length=256, verbose_name='Full name', blank=True)
-
+    sex_choices = (('M', "Male"), ("F", "Female"))
+    sex = models.CharField(max_length=10, verbose_name='Sex', choices=sex_choices, null=True)
     # REQUIRED_FIELDS = ['password', 'full_name']
+
+
+class Story(TimeStampedModel):
+    picture = models.URLField(verbose_name='Status Picture Url')
+    caption = models.CharField(max_length=256, verbose_name='Caption')
+
+    def save(self, *args, **kwargs):
+        super(Story, self).save(*args, **kwargs)
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    stories = models.ManyToManyField(Story, verbose_name='Stories')
+    # interests = models.
