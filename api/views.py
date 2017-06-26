83fa2ef825441981ac006318c2b62da7bd7ca1fa
Obi Uchenna David <daviduchenna@outlook.com>
2017-06-26 14:50:34 +0100

    sign up view

--- api/views.py
+++ api/views.py
@@ -1,11 +1,15 @@
 import json
 
 from django.contrib.auth import authenticate, login
+from django.http import JsonResponse
 from django.shortcuts import render
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView, Response
 from rest_framework.decorators import api_view
 from rest_framework.authentication import *
 from rest_framework.authtoken.models import Token
+
+from api.models import User
 from .serializers import UserSerializer
 
 
@@ -38,3 +42,40 @@ def login_user(request):
             'code': 101
         }
     return Response(status=200, data=data)
+
+
+class Suggestions(APIView):
+    authentication_classes = (BasicAuthentication, TokenAuthentication)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
+
+
+class Matches(APIView):
+    authentication_classes = (BasicAuthentication, TokenAuthentication)
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        pass
+
+
+class UserView(APIView):
+    def post(self, request):
+        print(request.data)
+
+        data = json.loads(str(request.data))
+        username = data['username']
+        full_name = data['full_name']
+        sex = data['sex']
+        password = data['password']
+
+        print(User.objects.filter(username=username).first())
+        if User.objects.filter(username=username).first() is not None:
+            context = {
+                "error": 102,
+                "message": "Username already taken"
+            }
+            return JsonResponse(data=context, status=200)
+
+        return JsonResponse(status=200, data="OK")
